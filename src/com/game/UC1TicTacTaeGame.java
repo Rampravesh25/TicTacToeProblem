e6556a58b38c48a662611d4c5f6805ca8c2707c7
Rampravesh25 <rampraveshmallah@gmail.com>
2022-05-25 19:31:41 +0530

    UC1 Program

---
+++ src/com/game/UC1TicTacTaeGame.java
@@ -0,0 +1,26 @@
+package com.game;
+
+public class UC1TicTacTaeGame {
+   void m1() {
+
+        char[] val=new char[10];
+        val[1]='A';
+    /*    val[2]=2;
+        val[3]=3;
+        val[4]=5;
+        val[5]=7;
+        val[6]=7;
+        val[7]=7;
+        val[8]=7;
+        val[9]=7;
+
+*/
+       System.out.println(val[1]);
+    }
+
+    public static void main(String[] args) {
+        UC1TicTacTaeGame uc = new UC1TicTacTaeGame();
+         uc.m1();
+
+    }
+}
